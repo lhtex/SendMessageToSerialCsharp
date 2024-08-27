8a87785747108a4a2289033dcdee80429e034d44
linco <linco@BEELINK>
2024-08-27 14:10:38 -0300

    Add project files.

---
+++ Project1-Teste/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Project1_Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox_serial = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.button_message = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox_serial
+            // 
+            this.comboBox_serial.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_serial.FormattingEnabled = true;
+            this.comboBox_serial.Location = new System.Drawing.Point(104, 32);
+            this.comboBox_serial.Name = "comboBox_serial";
+            this.comboBox_serial.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_serial.TabIndex = 0;
+            this.comboBox_serial.DropDown += new System.EventHandler(this.comboBox_serial_DropDown);
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(231, 31);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_connect.TabIndex = 1;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serial:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Location = new System.Drawing.Point(104, 90);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(121, 20);
+            this.textBox_message.TabIndex = 4;
+            // 
+            // button_message
+            // 
+            this.button_message.Location = new System.Drawing.Point(231, 89);
+            this.button_message.Name = "button_message";
+            this.button_message.Size = new System.Drawing.Size(75, 23);
+            this.button_message.TabIndex = 5;
+            this.button_message.Text = "Send";
+            this.button_message.UseVisualStyleBackColor = true;
+            this.button_message.Click += new System.EventHandler(this.button_message_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 147);
+            this.Controls.Add(this.button_message);
+            this.Controls.Add(this.textBox_message);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.comboBox_serial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox_serial;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Button button_message;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Project1-Teste/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Project1_Teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_serial_DropDown(object sender, EventArgs e)
+        {
+            string[] portlists = SerialPort.GetPortNames();
+            comboBox_serial.Items.Clear();
+            comboBox_serial.Items.AddRange(portlists);
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox_serial.Text;
+                serialPort1.BaudRate = 57600;
+                serialPort1.Open();
+
+                MessageBox.Show("Success Connected to Arduino Board" + "\n" +
+                    "This App Created by LHT" + "\n");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string txtMessage = "";
+
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(txtMessage + "\n");
+            }
+        }
+
+        private void button_message_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(textBox_message.Text + "\n");
+        }
+    }
+}
